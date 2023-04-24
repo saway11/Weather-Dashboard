3ec5e91f03cc0cb1de7a63b33f8f5566719c4094
unknown <rgarcia20.m@gmail.com>
2023-04-23 18:43:23 -0700

    added elements

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,7 +5,7 @@ var searchHistoryEl = document.getElementById("search-history")
 var currentContainerEl = document.getElementById("current-container")
 var forecastContainerEl = document.getElementById("forecast-container")
 
-var APIkey = "1af8ca56afa425104e46913689c7da68c";
+var APIkey = "1f40db029d8aa5986ddf3ab9927c8d74";
 var cities = []
 
 var loadCities = function() {
@@ -40,4 +40,70 @@ var displaySearchedCities = function(city) {
     });
 
     searchHistoryEl.appendChild(cityCardEl)
+}
+
+var displayCurrentData = function(city, data) {
+
+    // Endpoints to display current data
+    var tempCurrent = Math.round(data.current.temp);
+    var humidity = Math.round(data.current.humidity);
+    var windSpee = data.current.wind_speed;
+    var uvIndex = data.current.uvi;
+    var inconCurrent = data.current.weather[0].icon;
+
+    // create HTML for city/date/icon
+    currentContainerEl.textContent = ""
+    currentContainerEl.setAttribute("class", "m-3 border col-10 text-center")
+    var divCityHeader = document.createElement("div")
+    var headerCityDate = document.createElement("h2");
+    var currentdate = moment().format("L");
+    var imageIcon = document.createElement("img");
+    imageIcon.setAttribure('src', "")
+    imageIcon.setAttribute('src', "https://openweathermap.org/img/wn/" + iconCurrent + "@2x.png")
+    headerCityDate.textContent = city + " (" + currentdate + ")";
+
+    // Append to container for current data
+    divCityHeader.appendChild(headerCityDate)
+    divCityHeader.appendChild(imageIcon)
+    currentContainerEl.appendChild(divCityHeader)
+
+    // create element to display weather data
+    var divCurrent = document.createElement("div")
+    var tempEl = document.createElement("p");
+    var humidityEl = document.createElement("p");
+    var windSpeedEl = document.createElement("p");
+    var uvIndexEl = document.createElement("p");
+    var uvIndexColorEl = document.createElement("span")
+    uvIndexColorEl.textContent = uvIndex
+
+    // color for background of UVindex depending on severity
+
+    if (uvIndex <= 4) {
+        uvIndexColorEl.setAttribute("class", "bg-success text-white p-2")
+    } else if (uvIndex <= 8) {
+        uvIndexColorEl.setAttribute("class", "bg-warning text-black p-2")
+    } else {
+        uvIndexColorEl.setAttribute("class", "bg-danger text-white p-2")
+    }
+
+    // add current weather data to page
+    tempEl.textContent = "Temperature: " + tempCurrent + "°F";
+    humidityEl.textContent = "Humidity: " + humidity + "%";
+    windSpeedEl.textContent = "Wind Speed: " + windSpeed + "MPH";
+    uvIndexEl.textContent = "UV Index: ";
+
+    uvIndexEl.appendChild(uvIndexColorEl)
+
+
+    // append elements to section
+    divCurrent.appendChild(tempEl);
+    divCurrent.appendChild(humidityEl);
+    divCurrent.appendChild(windSpeedEl);
+    divCurrent.appendChild(uvIndexEl);
+
+    currentContainerEl.appendChild(divCurrent);
+};
+
+var displayForecastData = function(data) {
+    console.log(data)
 }
